bb4d60d8e21a9e6ce757ab69147b663ee2c3b379
Mayank Mishra <mi5294759@gmail.com>
2026-04-21 19:03:14 +0530

    Add files via upload

---
+++ Chapter 03/Chapter03.py
@@ -0,0 +1,37 @@
+# Loops :- 
+# while loop = execute some code WHILE some condition remains true
+
+
+# name = input("Enter your name : ")
+# while name == "":
+#     print("Please enter your name : ")
+#     name = input("Enter your name : ")
+    
+# print(f"Hello {name} , Welcome in Chapter 3")
+
+
+# food = input("Enter your favourite food (q to quit) : ")
+# while not food == "q":
+#     print(f"You Like {food}")
+#     food = input("Enter another food you like (q to quit) :")
+    
+# print("Thank you for sharing your favourite food with us \n  Byyye")
+
+
+
+# Intrust calculalter :- 
+
+
+            
+    
+    
+
+    
+    
+    
+    
+    
+
+
+
+
